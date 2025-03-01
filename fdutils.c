b886a3031d4893a07f022ed9d49298246ac40e4f
Aymane Benkaroum <abenkaro@e1r5p5.1337.ma>
2025-03-01 01:09:09 +0000

    aight

--- fdutils.c
+++ fdutils.c
@@ -34,12 +34,24 @@ char	*fetch_path(char *cmd, char **env)
 {
 	char	**possible_paths;
 	int		i;
+	char	*envpath;
+	char	*temp;
 
 	i = -1;
 	while(env[++i])
 	{
-		if (ft_strnstr(env[i], "PATH=", 5))
-			return (env[i] + 5);
+		if(ft_strncmp(env[i], "PATH=", 5) == 0)
+		{
+			envpath = &(*(env[i])) + 5;
+			possible_paths = ft_split(envpath, ':');
+			break;
+		}
+	}
+	i = -1;
+	while (possible_paths[++i])
+	{
+		temp = ft_strjoin(possible_paths[i], "/");
+
 	}
 	return (NULL);
 }
@@ -62,7 +74,7 @@ char	*abs_path(char *arg, char **env)
 	if (!name)
 		return (NULL);
 	if (!ft_strchr(name, '/'));
-		cmd = fetch_path(name, env);
+		return(fetch_path(name, env));
 	if (access(name, X_OK) == 0)
 		return (name);
 	return (NULL);
