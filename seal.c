4acc5aa658eea98f38cb259ba24f51e33e9160ba
Aymane Benkaroum <abenkaro@e1r6p2.1337.ma>
2025-03-05 00:51:58 +0000

    norm

--- seal.c
+++ seal.c
@@ -5,56 +5,64 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenkaro <abenkaro@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/04 21:53:44 by abenkaro          #+#    #+#             */
-/*   Updated: 2025/03/04 21:59:31 by abenkaro         ###   ########.fr       */
+/*   Created: 2025/03/05 00:51:05 by abenkaro          #+#    #+#             */
+/*   Updated: 2025/03/05 00:51:08 by abenkaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void unseal(char *params)
+void	unseal(char *params)
 {
-    int i = 0;
-    
-    while (params[i])
-    {
-        if (params[i] == '\x1F')
-            params[i] = ' ';
-        i++;
-    }
+	int	i;
+
+	i = 0;
+	while (params[i])
+	{
+		if (params[i] == '\x1F')
+			params[i] = ' ';
+		i++;
+	}
 }
 
-void trim_quotes(char **params)
+void	trim_quotes(char **params)
 {
-    char *temp;
-    
-    while (*params)
-    {
-        if (**params == '\'' || **params == '\"')
-        {
-            temp = ft_strtrim(*params, "\'\"");
-            if (!temp)
-                exit(1);
-            free(*params);
-            *params = temp;
-        }
+	char	*temp;
+
+	while (*params)
+	{
+		if (**params == '\'' || **params == '\"')
+		{
+			temp = ft_strtrim(*params, "\'\"");
+			if (!temp)
+				exit(1);
+			free(*params);
+			*params = temp;
+		}
 		unseal(*params);
-        params++;
-    }
+		params++;
+	}
 }
 
-void seal(char *params)
+void	seal(char *params)
 {
-    int i = 0;
-    int in_quotes = 0;
-    
-    while (params[i])
-    {
-        if (params[i] == '\'' || params[i] == '\"')
-            in_quotes = !in_quotes;
-        if (in_quotes && params[i] == ' ')
-            params[i] = '\x1F';
-        i++;
-    }
-}
+	int	i;
+	int	in_quotes;
+	int	in_dquotes;
 
+	in_quotes = 0;
+	in_dquotes = 0;
+	i = 0;
+	while (params[i])
+	{
+		if (params[i] == '\'')
+			in_quotes = !in_quotes;
+		if (in_quotes && params[i] == ' ')
+			params[i] = '\x1F';
+		else if (params[i] == '\"')
+			in_dquotes = !in_dquotes;
+		if (in_dquotes && params[i] == ' ')
+			params[i] = '\x1F';
+		i++;
+	}
+}
